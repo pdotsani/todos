7b5751f1f786f95906b3b4b7a59bee2aa35ba9e7
Patrick San Juan <pat.sanjuan@gmail.com>
2016-05-24 15:37:29 -0700

    create images namespace, post route

--- routers/router.go
+++ routers/router.go
@@ -10,5 +10,18 @@ func init() {
 	beego.SetStaticPath("/bower", "bower_components")
 	beego.SetStaticPath("/npm", "node_modules")
 
+	// Create server api namespace
+	// https://github.com/shlomizadok/beego_react/blob/master/routers/router.go
+	api := beego.NewNamespace("/api",
+		beego.NSNamespace("/images",
+			beego.NSInclude(
+				&controllers.ImageController{},
+			),
+		),
+	)
+
+	// Make api routes available for requests in app
+	beego.AddNamespace(api)
+	// Route to index.tpl
 	beego.Router("/", &controllers.MainController{})
 }
