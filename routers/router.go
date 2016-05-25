5753d4f4f718ea499cc5366ea53d54b739e8a0f0
Patrick San Juan <pat.sanjuan@gmail.com>
2016-05-25 14:01:45 -0700

    fixed api routing issue

--- routers/router.go
+++ routers/router.go
@@ -10,18 +10,8 @@ func init() {
 	beego.SetStaticPath("/bower", "bower_components")
 	beego.SetStaticPath("/npm", "node_modules")
 
-	// Create server api namespace
-	// https://github.com/shlomizadok/beego_react/blob/master/routers/router.go
-	api := beego.NewNamespace("/api",
-		beego.NSNamespace("/images",
-			beego.NSInclude(
-				&controllers.ImageController{},
-			),
-		),
-	)
-
-	// Make api routes available for requests in app
-	beego.AddNamespace(api)
+	// Images
+	beego.Router("/api/images", &controllers.ImageController{})
 	// Route to index.tpl
 	beego.Router("/", &controllers.MainController{})
 }
