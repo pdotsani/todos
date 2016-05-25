faa9f5df49f8369637e1b5efdfb2765ebee5c3aa
Patrick San Juan <pat.sanjuan@gmail.com>
2016-05-25 14:08:04 -0700

    created POST and GET tests for /api/images/

--- tests/default_test.go
+++ tests/default_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"bytes"
 	"github.com/astaxie/beego"
 	. "github.com/smartystreets/goconvey/convey"
 	"gopkg.in/jmcvetta/neoism.v1"
@@ -45,6 +46,41 @@ func TestNeo4j(t *testing.T) {
 		})
 		Convey("Db Connect Response", func() {
 			So(db, ShouldNotEqual, nil)
+		})
+	})
+}
+
+func TestImages(t *testing.T) {
+	// @POST
+	img := []byte(`{"alt":"image","url":"http://someurl.net/image.jpeg"}`)
+	r, _ := http.NewRequest("POST", "/api/images", bytes.NewBuffer(img))
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	beego.Trace("images", "TestImages", "Code[%d]\n%s", w.Code, w.Body.String())
+
+	Convey("Subject: Test POST /api/images\n", t, func() {
+		Convey("Status Code Should Be 201", func() {
+			So(w.Code, ShouldEqual, 201)
+		})
+		Convey("The Result Should Not Be Empty", func() {
+			So(w.Body.Len(), ShouldBeGreaterThan, 0)
+		})
+	})
+
+	// @GET
+	r2, _ := http.NewRequest("GET", "/api/images", nil)
+	w2 := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w2, r2)
+
+	beego.Trace("images", "TestImages", "Code[%d]\n%s", w2.Code, w2.Body.String())
+
+	Convey("Subject: Test GET /api/images\n", t, func() {
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+		Convey("The Result Should Not Be Empty", func() {
+			So(w.Body.Len(), ShouldBeGreaterThan, 0)
 		})
 	})
 }
